0493edfae105148c4da6c84ec8b6d7aa5612eddb
sokunthearithmakara <smakara@hawaii.edu>
2026-04-26 16:05:32 +0700

    fix: Correct release version from 1.0 to 0.1 in version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'mod_flexbook';
-$plugin->release      = '1.0';
+$plugin->release      = '0.1';
 $plugin->version      = 2026042008;
 $plugin->requires = 2021112800;
 $plugin->supported = [400, 502];

f117883024e54a6c5fa3bc3f9af7f762acbbecaa
sokunthearithmakara <smakara@hawaii.edu>
2026-08-10 09:55:26 +0700

    Release 1.2.1: Update versioning and enhance shop link for content types.
    - Updated plugin version to 1.2.1 and version number for compatibility.
    - Modified shop link to include a tag for better tracking of Flexbook-related content types.

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'mod_flexbook';
-$plugin->release      = '1.2';
-$plugin->version      = 2026080100;
+$plugin->release      = '1.2.1';
+$plugin->version      = 2026081000;
 $plugin->requires = 2021112800;
 $plugin->supported = [400, 502];
 $plugin->maturity = MATURITY_STABLE;
